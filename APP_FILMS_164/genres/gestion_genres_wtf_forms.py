d3d73be215f1681d6ab4389df4831705a6161307
flkdamien <damienfluck516@gmail.com>
2024-05-08 10:32:35 +0200

    Changement de la ligne 109 ERREUR id peut pas etre null

--- APP_FILMS_164/genres/gestion_genres_wtf_forms.py
+++ APP_FILMS_164/genres/gestion_genres_wtf_forms.py
@@ -16,7 +16,7 @@ class FormWTFAjouterGenres(FlaskForm):
         Définition d'un "bouton" submit avec un libellé personnalisé.
     """
     nom_genre_regexp = "^([A-Z]|[a-zÀ-ÖØ-öø-ÿ])[A-Za-zÀ-ÖØ-öø-ÿ]*['\- ]?[A-Za-zÀ-ÖØ-öø-ÿ]+$"
-    nom_genre_wtf = StringField("Clavioter le genre ", validators=[Length(min=2, max=20, message="min 2 max 20"),
+    nom_genre_wtf = StringField("nom ", validators=[Length(min=2, max=20, message="min 2 max 20"),
                                                                    Regexp(nom_genre_regexp,
                                                                           message="Pas de chiffres, de caractères "
                                                                                   "spéciaux, "
